50dff003d0d674a265a058f83a5d280f32c8743b
Nanaryu <herobrines120@gmail.com>
2026-01-26 09:48:06 +0100

    add texture

--- .godot/mono/temp/obj/Debug/Mango.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Mango.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30073c5e31f761731e73a3e8d08e9a637233cffa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ae7624a9cad63edb8b59c58cc1d6f747ac4829d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
