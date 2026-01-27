8d6dfd1ef2f9dcc127d830e2b91fe565df5b50cb
Szymon-Przeklasa <s.przeklasa.tp22@zsme.tarnow.pl>
2026-01-27 10:23:56 +0100

    .

--- StartButton.cs
+++ StartButton.cs
@@ -6,7 +6,8 @@ public partial class StartButton : TextureButton
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
+		//this.PivotOffsetRatio = new Vector2(0.5f, 0.5f);
+		//this.Position = new Vector2(728f, 488f);
 	}
 	private void StartGame()
 	{
@@ -14,11 +15,11 @@ public partial class StartButton : TextureButton
 	}
 	private void MouseOn()
 	{
-		//this.Scale = new Vector2(0.45f, 0.45f);
+		this.Scale = new Vector2(0.45f, 0.45f);
 	}
 	private void MouseOff()
 	{
-		//this.Scale = new Vector2(0.4f, 0.4f);
+		this.Scale = new Vector2(0.4f, 0.4f);
 	}
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
