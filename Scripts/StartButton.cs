2136cc9dd2e4d796535477a6335ade3ad03601dd
Szymon-Przeklasa <s.przeklasa.tp22@zsme.tarnow.pl>
2026-01-28 11:24:12 +0100

    added button anim

--- Scripts/StartButton.cs
+++ Scripts/StartButton.cs
@@ -6,8 +6,7 @@ public partial class StartButton : TextureButton
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		//this.PivotOffsetRatio = new Vector2(0.5f, 0.5f);
-		//this.Position = new Vector2(728f, 488f);
+		
 	}
 	private void StartGame()
 	{
@@ -15,11 +14,11 @@ public partial class StartButton : TextureButton
 	}
 	private void MouseOn()
 	{
-		this.Scale = new Vector2(0.45f, 0.45f);
+		this.Modulate = new Color(0.8f,0.8f,0.8f,1f);
 	}
 	private void MouseOff()
 	{
-		this.Scale = new Vector2(0.4f, 0.4f);
+		this.Modulate = new Color(1f,1f,1f,1f);
 	}
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
